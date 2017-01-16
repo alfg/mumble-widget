6e6e23d34dc807a8af8e18b22ea853eacb2fc337
Alf <alf.g.jr@gmail.com>
2017-01-15 20:09:44 -0800

    Update distribution.

--- dist/mumble-widget.cdn.js
+++ dist/mumble-widget.cdn.js
@@ -87,14 +87,34 @@
                 </thead><tbody> \
                 <!-- ko if: $data.root --> \
                   <!-- ko foreach: root.users --> \
-                  <tr class='root-users'><td data-bind='text: name'></td></tr> \
+                  <tr class='root-users'> \
+                      <td> \
+                          <span data-bind='text: name'></span> \
+                          <span data-bind='css: { &quot;mute-on&quot;: mute }'></span> \
+                          <span data-bind='css: { &quot;deaf-on&quot;: deaf }'></span> \
+                          <span data-bind='css: { &quot;self-mute-on&quot;: selfMute }'></span> \
+                          <span data-bind='css: { &quot;self-deaf-on&quot;: selfDeaf }'></span> \
+                          <span data-bind='css: { &quot;priority-speaker-on&quot;: prioritySpeaker }'></span> \
+                          <span data-bind='css: { &quot;recording-on&quot;: recording }'></span> \
+                      </td> \
+                  </tr> \
                   <!-- /ko --> \
                   <!-- ko foreach: root.channels --> \
                     <!-- ko if: users.length > 0 --> \
                     <tr class='subchannels'><td data-bind='text: name'></td></tr> \
                     <!-- /ko --> \
                     <!-- ko foreach: users --> \
-                    <tr class='sub-users'><td data-bind='text: &apos;&mdash; &apos; + name'></td></tr> \
+                    <tr class='sub-users'> \
+                      <td> \
+                          <span data-bind='text: &apos;&mdash; &apos; + name'></span> \
+                          <span data-bind='css: { &quot;mute-on&quot;: mute }'></span> \
+                          <span data-bind='css: { &quot;deaf-on&quot;: deaf }'></span> \
+                          <span data-bind='css: { &quot;self-mute-on&quot;: selfMute }'></span> \
+                          <span data-bind='css: { &quot;self-deaf-on&quot;: selfDeaf }'></span> \
+                          <span data-bind='css: { &quot;priority-speaker-on&quot;: prioritySpeaker }'></span> \
+                          <span data-bind='css: { &quot;recording-on&quot;: recording }'></span> \
+                      </td> \
+                    </tr> \
                     <!-- /ko --> \
                     <!-- ko template: {name: 'subchannels_template', foreach: $data.channels} --> \
                     <!-- /ko --> \
@@ -112,7 +132,17 @@
                 <script id='subchannels_template' type='text/html'> \
                         <tr class='subchannels'><td data-bind='text: &apos;&mdash; &apos; + name, visible: users.length > 0'></td></tr> \
                         <!-- ko foreach: users --> \
-                        <tr class='sub-users'><td data-bind='text: &apos;&mdash; &apos; + name'></td></tr> \
+                        <tr class='sub-users'> \
+                          <td> \
+                              <span data-bind='text: &apos;&mdash; &apos; + name'></span> \
+                              <span data-bind='css: { &quot;mute-on&quot;: mute }'></span> \
+                              <span data-bind='css: { &quot;deaf-on&quot;: deaf }'></span> \
+                              <span data-bind='css: { &quot;self-mute-on&quot;: selfMute }'></span> \
+                              <span data-bind='css: { &quot;self-deaf-on&quot;: selfDeaf }'></span> \
+                              <span data-bind='css: { &quot;priority-speaker-on&quot;: prioritySpeaker }'></span> \
+                              <span data-bind='css: { &quot;recording-on&quot;: recording }'></span> \
+                          </td> \
+                        </tr> \
                         <!-- /ko --> \
                         <!-- ko template: {name: 'subchannels_template', foreach: $data.channels} --> \
                         <!-- /ko --> \
